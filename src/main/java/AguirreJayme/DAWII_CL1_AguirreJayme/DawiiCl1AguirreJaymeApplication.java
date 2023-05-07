96011f7e386ac56797e68b1605cf5cda90872dc8
jaime <jaymeaguirre26@gmail.com>
2023-05-06 21:27:47 -0500

    Initial commit

---
+++ src/main/java/AguirreJayme/DAWII_CL1_AguirreJayme/DawiiCl1AguirreJaymeApplication.java
@@ -0,0 +1,13 @@
+package AguirreJayme.DAWII_CL1_AguirreJayme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1AguirreJaymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1AguirreJaymeApplication.class, args);
+	}
+
+}
